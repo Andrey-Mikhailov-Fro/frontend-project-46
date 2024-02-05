fbb308b76f1d620481188e1c53403b085bdc6cc8
Andrey-Mikhailov-Fro <dark.ace.frost@gmail.com>
2024-02-05 13:49:08 +0300

    done simple diff, complete step 4

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,19 +1,30 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import getFiles from './getFiles.js';
+import _ from 'lodash';
+import genDiff from './getdiff.js';
 
-/*program
-    .option('-h, --help', 'output usage information')
-    .option('-v, --version', 'output version number');
-*/
+const doDiff = () => {
+  const command = (filepath1, filepath2) => {
+    const collection = [filepath1, filepath2];
+    const [file1, file2] = collection.map((item) => getFiles(item));
+    console.log(genDiff(file1, file2));
+  };
 
-program
+  program
     .name('gendiff')
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>');
+    .arguments('<filepath1> <filepath2>')
+    .action(command);
 
-program
+  program
     .option('-f, --format [type]', 'output format');
 
-program.parse();
+  program.parse();
+};
+
+doDiff();
+
+export default doDiff;
---
+++ src/getFiles.js
@@ -0,0 +1,13 @@
+import * as path from 'node:path';
+import process from 'node:process';
+import * as fs from 'node:fs';
+
+const getFiles = (way) => {
+    const actualPath = path.resolve(process.cwd(), way);
+
+    const fileContent = fs.readFileSync(actualPath);
+
+    return JSON.parse(fileContent);
+};
+
+export default getFiles;
