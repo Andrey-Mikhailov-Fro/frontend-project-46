fd6062d4167e21b9dc75b2b7ddd8f76b0e8c91c8
Andrey-Mikhailov-Fro <dark.ace.frost@gmail.com>
2024-03-10 20:27:19 +0300

    refactoring, try #3.7

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,38 +1,33 @@
 import _ from 'lodash';
 
+const getValue = (node) => {
+  if (_.isObject(node)) {
+    return '[complex value]';
+  }
+
+  if (typeof node === 'string') {
+    return `'${node}'`;
+  }
+
+  return node;
+};
+
 const doPlainDiff = (diff, currentPath = '', depth = 1) => {
-  const getValue = (node) => {
-    if (_.isObject(node)) {
-      return '[complex value]';
-    }
-
-    if (typeof node === 'string') {
-      return `'${node}'`;
-    }
-
-    return node;
-  };
-
   const plainedDiff = diff.flatMap((item) => {
     const stringifiedPath = depth === 1 ? `${item.property}` : `${currentPath}.${item.property}`;
 
-    if (item.state === 'deleted') {
-      return `Property '${stringifiedPath}' was removed`;
+    switch (item.state) {
+      case 'deleted':
+        return `Property '${stringifiedPath}' was removed`;
+      case 'added':
+        return `Property '${stringifiedPath}' was added with value: ${getValue(item.value)}`;
+      case 'complex':
+        return doPlainDiff(item.children, stringifiedPath, depth + 1);
+      case 'changed':
+        return `Property '${stringifiedPath}' was updated. From ${getValue(item.file1Value)} to ${getValue(item.file2Value)}`;
+      default:
+        return null;
     }
-
-    if (item.state === 'added') {
-      return `Property '${stringifiedPath}' was added with value: ${getValue(item.value)}`;
-    }
-
-    if (item.state === 'complex') {
-      return doPlainDiff(item.children, stringifiedPath, depth + 1);
-    }
-
-    if (item.state === 'changed') {
-      return `Property '${stringifiedPath}' was updated. From ${getValue(item.file1Value)} to ${getValue(item.file2Value)}`;
-    }
-
-    return null;
   }).filter((plainedString) => plainedString !== null).join('\n');
 
   return plainedDiff;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,50 +1,45 @@
 import _ from 'lodash';
 
+const getValue = (data, innerDepth) => {
+  if (data === null) {
+    return 'null';
+  }
+
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
+
+  const innerSpace = ' '.repeat(4 * innerDepth);
+  const outerSpace = ' '.repeat(4 * (innerDepth - 1));
+
+  const benches = Object.entries(data).map(([key, value]) => {
+    const reachedValue = getValue(value, innerDepth + 1);
+    const bench = `${innerSpace}${key}: ${reachedValue}`;
+    return bench;
+  });
+
+  const innerTree = `{\n${benches.join('\n')}\n${outerSpace}}`;
+
+  return innerTree;
+};
+
 const generateTree = (diff, depth = 1) => {
   const space = ' '.repeat(4 * depth - 3);
   const lockSpace = ' '.repeat(4 * (depth - 1));
 
-  const getValue = (data, innerDepth) => {
-    if (data === null) {
-      return 'null';
-    }
-
-    if (!_.isObject(data)) {
-      return `${data}`;
-    }
-
-    const innerSpace = ' '.repeat(4 * innerDepth);
-    const outerSpace = ' '.repeat(4 * (innerDepth - 1));
-
-    const benches = Object.entries(data).map(([key, value]) => {
-      const reachedValue = getValue(value, innerDepth + 1);
-      const bench = `${innerSpace}${key}: ${reachedValue}`;
-      return bench;
-    });
-
-    const innerTree = `{\n${benches.join('\n')}\n${outerSpace}}`;
-
-    return innerTree;
-  };
-
   const tree = diff.flatMap((node) => {
-    if (node.state === 'deleted') {
-      return `${space}- ${node.property}: ${getValue(node.value, depth + 1)}`;
+    switch (node.state) {
+      case 'deleted':
+        return `${space}- ${node.property}: ${getValue(node.value, depth + 1)}`;
+      case 'added':
+        return `${space}+ ${node.property}: ${getValue(node.value, depth + 1)}`;
+      case 'complex':
+        return `${space}  ${node.property}: ${generateTree(node.children, depth + 1)}`;
+      case 'cahnged':
+        return [`${space}- ${node.property}: ${getValue(node.file1Value, depth + 1)}`, `${space}+ ${node.property}: ${getValue(node.file2Value, depth + 1)}`];
+      default:
+        return `${space}  ${node.property}: ${getValue(node.value)}`;
     }
-
-    if (node.state === 'added') {
-      return `${space}+ ${node.property}: ${getValue(node.value, depth + 1)}`;
-    }
-
-    if (node.state === 'complex') {
-      return `${space}  ${node.property}: ${generateTree(node.children, depth + 1)}`;
-    }
-
-    if (node.state === 'changed') {
-      return [`${space}- ${node.property}: ${getValue(node.file1Value, depth + 1)}`, `${space}+ ${node.property}: ${getValue(node.file2Value, depth + 1)}`];
-    }
-
-    return `${space}  ${node.property}: ${getValue(node.value)}`;
   });
 
   const completedTree = depth === 1 ? `{\n ${tree.join('\n ')}\n}` : `{\n ${tree.join('\n ')}\n${lockSpace}}`;
